1950104447a7601af1625ad326986993c38e1a4e
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-11-12 20:52:12 +0700

    Update perceptron_pacman.py

--- classification/perceptron_pacman.py
+++ classification/perceptron_pacman.py
@@ -45,7 +45,12 @@ class PerceptronClassifierPacman(PerceptronClassifier):
         self.features = trainingData[0][0]['Stop'].keys() # could be useful later
         # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
         # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
+        """
+        Thực hiện thuật toán đã sửa đổi perceptron cho pacman. 
+        Không giống như perceptron cho các số, tất cả các nhãn đều dùng chung 1 vector trọng số weight
+        Implement thuật toán theo guide
 
+        """
         for iteration in range(self.max_iterations):
             print "Starting iteration ", iteration, "..."
             for X, y in zip(trainingData, trainingLabels):
