15de8cadf23898af8837c722469be21a79c06f83
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-01-04 16:24:48 +0700

    Update
    Finished at 16:22:29
    
    Provisional grades
    ==================
    Question q1: 6/6
    ------------------
    Total: 6/6

--- reinforcement/valueIterationAgents.py
+++ reinforcement/valueIterationAgents.py
@@ -1,14 +1,15 @@
 # valueIterationAgents.py
 # -----------------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# Licensing Information:  You are free to use or extend these projects for 
+# educational purposes provided that (1) you do not distribute or publish 
+# solutions, (2) you retain this notice, and (3) you provide clear 
+# attribution to UC Berkeley, including a link to 
+# http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
 # 
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
+# The core projects and autograders were primarily created by John DeNero 
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
+# Student side autograding was added by Brad Miller, Nick Hay, and 
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
@@ -19,7 +20,6 @@ from learningAgents import ValueEstimationAgent
 class ValueIterationAgent(ValueEstimationAgent):
     """
         * Please read learningAgents.py before reading this.*
-
         A ValueIterationAgent takes a Markov decision process
         (see mdp.py) on initialization and runs value iteration
         for a given number of iterations using the supplied
@@ -30,7 +30,6 @@ class ValueIterationAgent(ValueEstimationAgent):
           Your value iteration agent should take an mdp on
           construction, run the indicated number of iterations
           and then act according to the resulting policy.
-
           Some useful mdp methods you will use:
               mdp.getStates()
               mdp.getPossibleActions(state)
@@ -45,7 +44,18 @@ class ValueIterationAgent(ValueEstimationAgent):
 
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
-
+        self.oldValues = self.values.copy()
+        states = mdp.getStates()
+        for i in range(iterations):
+            for state in states:
+                actions = mdp.getPossibleActions(state)
+                if not mdp.isTerminal(state):
+                    actionValue = -float('inf')
+                    for action in actions:
+                        qValue = self.computeQValueFromValues(state, action)
+                        actionValue = max(actionValue, qValue)
+                    self.values[state] = actionValue
+            self.oldValues = self.values.copy()
 
     def getValue(self, state):
         """
@@ -60,19 +70,27 @@ class ValueIterationAgent(ValueEstimationAgent):
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        qValue = 0
+        for nextState, prob in self.mdp.getTransitionStatesAndProbs(state, action):
+            qValue += prob * (self.discount * self.oldValues[nextState] + self.mdp.getReward(state, action, nextState))
+        return qValue
 
     def computeActionFromValues(self, state):
         """
           The policy is the best action in the given state
           according to the values currently stored in self.values.
-
           You may break ties any way you see fit.  Note that if
           there are no legal actions, which is the case at the
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions, maxValue, decision = self.mdp.getPossibleActions(state), -float('inf'), None
+        for action in actions:
+            actionValue = self.computeQValueFromValues(state, action)
+            if actionValue > maxValue:
+                maxValue = actionValue
+                decision = action
+        return decision
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
